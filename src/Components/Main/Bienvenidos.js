71ffd446cfb204adb5e27aa725eb02f8068feb7d
Giovanni <giovannibarolin@gmail.com>
2022-12-20 21:57:23 -0300

    Modificando pagina de bienvenida

--- src/Components/Main/Bienvenidos.js
+++ src/Components/Main/Bienvenidos.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+const boxStyle={
+    display:"flex",
+    flexDirection: "column",
+    justifyContent:"space-around",
+    alignItems:"center",
+    width:{xs:"200px",md:"auto"},
+    height:"200px",
+    margin:{xs:0,md:"50px 150px 0 0"}
+}
+
 function Bienvenidos(){
-
-
+    const theme = useTheme();
+    const sm = useMediaQuery(theme.breakpoints.down("sm"));
+    console.log(sm);
     return(
 
-            <div className="bienvenidos">
-                <Typography variant="h3">BIENVENIDOS A CB3DESIGN ROSARIO</Typography> 
-            </div>
+            <Box className="bienvenidos">
+                <Box sx={boxStyle}>
+                    <Typography color="secondary" variant={sm ? "subtitle1" : "h5"}>BIENVENIDOS</Typography>
+                    <Typography color="secondary" variant={sm ? "h3" : "h1"}>CB3DESIGN</Typography>
+                    <Typography color="secondary" align="center" variant={sm ? "subtitle1" : "h5"}>Venta de accesorios para repostería</Typography>
+                </Box>
+            </Box>
 
     );
 }
